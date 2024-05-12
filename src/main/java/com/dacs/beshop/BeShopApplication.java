8d2a0f78e4847cbf006962764edd2b2fd26e8043
tuanworlddev <nanhtuan.it@gmail.com>
2024-05-12 09:48:46 +0700

    Initial commit

---
+++ src/main/java/com/dacs/beshop/BeShopApplication.java
@@ -0,0 +1,13 @@
+package com.dacs.beshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dacs/beshop/BeShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dacs.beshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
